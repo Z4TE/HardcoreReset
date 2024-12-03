820a393469ed57cebdccf94d641574b5723c29bc
Z4TE <kayakun426@gmail.com>
2024-12-03 12:29:51 +0900

    Players will no longer be kicked while in survival mode.

--- src/main/java/net/z4te/reset/Main.java
+++ src/main/java/net/z4te/reset/Main.java
@@ -11,13 +11,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin implements Listener {
 
-    String displayMotd = getConfig().getString("motd") + getConfig().getString("death-message");
+    String motd = getConfig().getString("motd");
+    String deathMessage = getConfig().getString("death-message");
+    String displayMotd = motd + deathMessage;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        assert displayMotd != null;
-        Bukkit.getServer().setMotd(displayMotd);
+
+        if (motd == null) {
+            // Set the default motd
+            Bukkit.getServer().setMotd(ChatColor.WHITE + "Edit motd in config.yml");
+        } else {
+            assert displayMotd != null;
+            Bukkit.getServer().setMotd(displayMotd);
+        }
 
         getServer().getPluginManager().registerEvents(this, this);
     }
@@ -29,15 +37,17 @@ public final class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        String cause = event.getDeathMessage();
-        Bukkit.getOnlinePlayers().forEach(player -> player.kickPlayer(ChatColor.RED + cause));
+        // Execute when a player dies in hardcore mode
 
         if (getServer().isHardcore()) {
+            String cause = event.getDeathMessage();
+            Bukkit.getOnlinePlayers().forEach(player -> player.kickPlayer(ChatColor.RED + cause));
             if (cause != null) {
                 getConfig().set("death-message", cause);
                 Bukkit.getServer().setMotd(displayMotd);
                 saveConfig();
             }
+            // stop
             Bukkit.shutdown();
         }
     }
